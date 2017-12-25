e44ae738b0bec67e458107cb88be3ea40c21c674
Akshay jore <joreakshay@gmail.com>
2017-12-25 23:17:38 +0530

    prime no,vowel count,range of odd no,som of odd no

---
+++ SimplePrograms/checkprime.py
@@ -0,0 +1,14 @@
+import math
+def isprime(no):
+   for x in range(3,(int (math.sqrt(no))+1),2):
+      if (no%x) ==0:
+         return False
+   return True
+def main():
+   no=eval(input("enter no:")
+   if isprime(no):
+      prime("no is prime")
+   else:
+      prime("no is not prime")
+if __name__=="__main__":
+   main()
