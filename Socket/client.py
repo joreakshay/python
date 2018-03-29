e6732a4933afdaf615616d834e6da10b51242103
Akshay Jore <34796363+joreakshay@users.noreply.github.com>
2018-03-29 19:26:57 +0530

    Update client.py

--- Socket/client.py
+++ Socket/client.py
@@ -7,35 +7,37 @@ class ServerInfo():
 		self.port=port
 		self.ststud_command=ststud_command
 		self.output_file=output_file
-	
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect the socket to the port where the server is listening
-server_address = ('192.168.1.102',10004)
-print('connecting to %s port %s' % server_address)
-sock.connect(server_address)
-
-try:
-    # Send data
-    message = 'India is my Country, all Indians are my brothers & sisters.'
-    print('sending "%s"' % message)
-    while 1:
-        #sock.send(bytes(message, 'UTF-8')) # 3 & above
-        sock.sendall(message) # 2.7
-    
-        # Look for the response
-        amount_received = 0
-        amount_expected = len(message)
-
-        while amount_received < amount_expected:
-            data = sock.recv(10)
-            amount_received += len(data)
-            print('received "%s"' % data)
-
-        message = input("Enter msg:-")
-
-finally:
-    print('closing socket')
-sock.close()
+		self.sock=""
+	def __del__(self):
+		del(self)
+	def connect(self):
+		# Create a TCP/IP socket
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		
+		# Connect the socket to the port where the server is listening
+		server_address = (self.ip,self.port)
+		print('connecting to %s port %s' % server_address)
+		self.sock.connect(server_address)
+		
+		try:
+			# Send data
+			message = 'India is my Country, all Indians are my brothers & sisters.'
+			print('sending "%s"' % message)
+			while 1:
+				#sock.send(bytes(message, 'UTF-8')) # 3 & above
+				sock.sendall(message) # 2.7
+			
+				# Look for the response
+				amount_received = 0
+				amount_expected = len(message)
+		
+				while amount_received < amount_expected:
+					data = sock.recv(10)
+					amount_received += len(data)
+					print('received "%s"' % data)
+		
+				message = input("Enter msg:-")
+		
+		finally:
+			print('closing socket')
+		sock.close()
